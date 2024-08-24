cd62c478265eb8f8d6f439178b2c6ca3a8e3f96d
Andrei Tokmakov <andtokm@yandex.ru>
2024-08-24 21:49:02 +0400

    24_08_2024_002

---
+++ MatchingEngine/MatchingEngine.h
@@ -0,0 +1,25 @@
+/**============================================================================
+Name        : MatchingEngine.h
+Created on  : 12.08.2024
+Author      : Andrei Tokmakov
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : MatchingEngine.cpp
+============================================================================**/
+
+#ifndef CPPPROJECTS_MATCHINGENGINE_H
+#define CPPPROJECTS_MATCHINGENGINE_H
+
+void OrderMatchingEngineTests();
+void MatchingEngine_WithAllocator_Tests();
+
+namespace MatchingEngine
+{
+    void TestAll()
+    {
+        OrderMatchingEngineTests();
+        // MatchingEngine_WithAllocator_Tests();
+    }
+}
+
+#endif //CPPPROJECTS_MATCHINGENGINE_H
--- MatchingEngine/main.cpp
+++ MatchingEngine/main.cpp
@@ -1,21 +1,24 @@
 /**============================================================================
-Name        : Tests.cpp
-Created on  : 15.09.2021
+Name        : main.cpp
+Created on  : 24.28.2024
 Author      : Andrei Tokmakov
 Version     : 1.0
 Copyright   : Your copyright notice
 Description : Tests C++ project
 ============================================================================**/
 
-#include <cstdlib>
+#include <optional>
 #include <iostream>
 #include <string_view>
-#include <vector>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <filesystem>
 
 #include "StringUtilities.h"
 
 
-int main([[maybe_unused]] const int argc,
+int main([[maybe_unused]] int argc,
          [[maybe_unused]] char** argv)
 {
     const std::vector<std::string_view> args(argv + 1, argv + argc);

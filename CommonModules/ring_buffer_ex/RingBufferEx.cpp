7e134c4fbc2d63889d7faa8c3dd928f4e4f6249c
Andrei.Tokmakov <andtokm@yandex.ru>
2025-04-07 19:31:32 +0400

    07_04_2024_01

--- CommonModules/ring_buffer_ex/RingBufferEx.cpp
+++ CommonModules/ring_buffer_ex/RingBufferEx.cpp
@@ -10,6 +10,7 @@ Description : RingBufferEx
 #include "RingBufferEx.h"
 #include <vector>
 #include <cstdint>
+#include <iostream>
 
 namespace
 {
@@ -27,28 +28,75 @@ namespace
     template<typename T, size_t Capacity>
     struct RingBuffer
     {
-        using size_type = size_t;
+        using size_type  = int_fast32_t;
         using value_type = T;
-        // using collection_type = std::vector<value_type>;
-        using collection_type = std::array<value_type, Capacity>;
+        using collection_type = std::vector<value_type>;
+        // using collection_type = std::array<value_type, Capacity>;
 
         static_assert(!std::is_same_v<value_type, void>, "ERROR: Value type can not be void");
         static_assert(is_pow_of_2(Capacity),  "ERROR: Capacity must be a power of 2");
 
         collection_type buffer {};
+        size_type idxWrite { 0 };
+        size_type idxRead { 0 };
+        bool full { false };
+
+        RingBuffer(): buffer(Capacity) {
+        }
 
         void put(value_type&& value)
         {
+            buffer[idxWrite] = std::move(value);
+
+            if (full) {
+                idxRead = fast_modulo(idxRead + 1, Capacity);
+            }
+
+            idxWrite = fast_modulo(idxWrite + 1, Capacity);
+            full = (idxWrite == idxRead);
         }
 
         bool get(value_type& value)
         {
+            if (true == empty())
+                return false;
+
+            value = std::move(buffer[idxRead]);
+            full = false;
+            idxRead = fast_modulo(idxRead + 1, Capacity);
+
             return true;
+        }
+
+        [[nodiscard]]
+        bool empty() const noexcept {
+            return (!full && (idxWrite == idxRead));
+        }
+
+        [[nodiscard]]
+        bool isFull() const noexcept {
+            return full;
+        }
+
+        static size_type capacity() noexcept {
+            return Capacity;
         }
     };
 }
 
+
+
 void RingBufferEx::TestAll()
 {
-    RingBuffer<int, 32> buffer;
+    RingBuffer<int, 16> buffer;
+
+    for (int i = 1; i <= 32; ++i)
+    {
+        buffer.put(std::move(i));
+    }
+
+    int value = 0;
+    while (buffer.get(value)) {
+        std::cout << value << " ";
+    }
 }

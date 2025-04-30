f3e55e586a65038bb0496e079a8ab847e5b96738
Andrei.Tokmakov <andtokm@yandex.ru>
2025-04-30 19:47:49 +0400

    30_04_2025_001

---
+++ Parsers_JSON/binance/BinanceDataParser.h
@@ -0,0 +1,18 @@
+/**============================================================================
+Name        : BinanceDataParser.h
+Created on  : 30.04.2025
+Author      : Andrei Tokmakov
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : BinanceDataParser.h
+============================================================================**/
+
+#ifndef FINANCETECHNOLOGYPROJECTS_BINANCEDATAPARSER_H
+#define FINANCETECHNOLOGYPROJECTS_BINANCEDATAPARSER_H
+
+namespace binance
+{
+    void TestAll();
+}
+
+#endif //FINANCETECHNOLOGYPROJECTS_BINANCEDATAPARSER_H

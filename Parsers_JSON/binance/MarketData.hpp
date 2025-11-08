ddf31b2803154d36f8fd26a53f7ce7dc6f2948f7
Andrei.Tokmakov <andtokm@yandex.ru>
2025-11-08 17:26:03 +0400

    08_11_2025_02

--- Parsers_JSON/binance/MarketData.hpp
+++ Parsers_JSON/binance/MarketData.hpp
@@ -11,6 +11,8 @@ Description :
 #define FINANCETECHNOLOGYPROJECTS_MARKETDATA_HPP
 
 #include <string_view>
+#include <format>
+#include <ostream>
 #include <cstdint>
 
 namespace binance::market_data
@@ -64,6 +66,114 @@ namespace binance::market_data
         static constexpr std::string_view aggTrade { "aggTrade" };
         static constexpr std::string_view trade { "trade" };
     };
+
+    struct StreamNames
+    {
+        static constexpr std::string_view trade { "trade" };
+        static constexpr std::string_view aggTrade { "aggTrade" };
+        static constexpr std::string_view ticker { "ticker" };
+        static constexpr std::string_view bookTicker { "bookTicker" };
+        static constexpr std::string_view depth { "depth" };
+        static constexpr std::string_view depth20 { "depth20" };
+        static constexpr std::string_view kline_1m { "kline_1m" };
+        static constexpr std::string_view miniTicker { "miniTicker" };
+    };
+}
+
+namespace binance::market_data
+{
+    /*{
+        "e": "24hrMiniTicker",  // Event type
+        "E": 1672515782136,     // Event time
+        "s": "BNBBTC",          // Symbol
+        "c": "0.0025",          // Close price
+        "o": "0.0010",          // Open price
+        "h": "0.0025",          // High price
+        "l": "0.0010",          // Low price
+        "v": "10000",           // Total traded base asset volume
+        "q": "18"               // Total traded quote asset volume
+    }*/
+
+    struct MiniTicker
+    {
+        std::string symbol {};
+        uint64_t timestamp { 0 };
+        Price    close { 0.0 };
+        Price    open { 0.0 };
+        Price    high { 0.0 };
+        Price    low { 0.0 };
+        double   volume { 0.0 };
+        Quantity quantity { 0.0 };
+    };
+
+    /*{
+      "u":400900217,     // order book updateId
+      "s":"BNBUSDT",     // symbol
+      "b":"25.35190000", // best bid price
+      "B":"31.21000000", // best bid qty
+      "a":"25.36520000", // best ask price
+      "A":"40.66000000"  // best ask qty
+    }*/
+
+    struct BookTicker
+    {
+        std::string symbol;
+        Price    bidPrice { 0.0 };
+        Quantity bidQuantity { 0.0 };
+        Price    askPrice { 0.0 };
+        Quantity askQuantity { 0.0 };
+        uint64_t updateId { 0 };
+    };
+}
+
+template<>
+struct std::formatter<binance::market_data::MiniTicker>
+{
+    static constexpr auto parse(const std::format_parse_context& ctx) -> decltype(auto) {
+        return ctx.begin();
+    }
+
+    static auto format(const binance::market_data::MiniTicker& miniTicker, std::format_context& ctx) -> decltype(auto)
+    {
+        return std::format_to(ctx.out(),
+            "MiniTicker(""\n\tsymbol: {},\n\ttimestamp: {}, \n\tclose: {},\n\topen: {},\n\thigh: {},\n\tlow: {},\n\tvolume: {},\n\tquantity: {}\n)",
+            miniTicker.symbol,
+            miniTicker.timestamp,
+            miniTicker.close,
+            miniTicker.open,
+            miniTicker.high,
+            miniTicker.low,
+            miniTicker.volume,
+            miniTicker.quantity);
+    }
+};
+
+template<>
+struct std::formatter<binance::market_data::BookTicker>
+{
+    static constexpr auto parse(const std::format_parse_context& ctx) -> decltype(auto) {
+        return ctx.begin();
+    }
+
+    static auto format(const binance::market_data::BookTicker& bookTicker, std::format_context& ctx) -> decltype(auto)
+    {
+        return std::format_to(ctx.out(),
+            "BookTicker(\n\tsymbol: {},\n\tbidPrice: {}, \n\tbidQuantity: {},\n\taskPrice: {},\n\taskQuantity: {},\n\tupdateId: {}\n)",
+            bookTicker.symbol,
+            bookTicker.bidPrice,
+            bookTicker.bidQuantity,
+            bookTicker.askPrice,
+            bookTicker.askQuantity,
+            bookTicker.updateId);
+    }
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const binance::market_data::BookTicker& ticker) {
+    return stream << std::format("{}", ticker);
+}
+
+inline std::ostream& operator<<(std::ostream& stream, const binance::market_data::MiniTicker& ticker) {
+    return stream << std::format("{}", ticker);
 }
 
 #endif //FINANCETECHNOLOGYPROJECTS_MARKETDATA_HPP
--- Parsers_JSON/binance/Parse_Experiments.cpp
+++ Parsers_JSON/binance/Parse_Experiments.cpp
@@ -12,13 +12,147 @@ Description :
 
 #include <vector>
 #include <iostream>
-#include <string_view>
-#include <format>
+#include <fstream>
+#include <variant>
+#include <filesystem>
 
 #include "FileUtilities.hpp"
+#include <nlohmann/json.hpp>
+
+namespace
+{
+    constexpr std::filesystem::path getDataDir() noexcept
+    {
+        return std::filesystem::current_path() / "../../Parsers_JSON/data/binance/";
+    }
+
+    std::vector<std::string> readFile(const std::filesystem::path &filePath)
+    {
+        std::vector<std::string> lines;
+        if (std::ifstream file(filePath); file.is_open() && file.good()){
+            while (std::getline(file, lines.emplace_back())) { /** **/ }
+        }
+        lines.pop_back();
+        return lines;
+    }
+}
+
+
+namespace parsing::mini_ticker
+{
+    using binance::market_data::MiniTicker;
+
+    MiniTicker parseMiniTicker(const nlohmann::json& data)
+    {
+        MiniTicker ticker;
+        data.at("E").get_to(ticker.timestamp);
+        ticker.symbol   = data.value("s", "");
+        ticker.close    = std::stod(data.value("c", "0"));
+        ticker.open     = std::stod(data.value("o", "0"));
+        ticker.high     = std::stod(data.value("h", "0"));
+        ticker.low      = std::stod(data.value("l", "0"));
+        ticker.volume   = std::stod(data.value("v", "0"));
+        ticker.quantity = std::stod(data.value("q", "0"));
+        return ticker;
+    }
+
+    MiniTicker parseMiniTicker2(const nlohmann::json& data)
+    {
+        MiniTicker ticker;
+        data.at("E").get_to(ticker.timestamp);
+        data.at("s").get_to(ticker.symbol);
+        ticker.close    = std::stod(data.at("c").get_ref<const std::string&>());
+        ticker.open     = std::stod(data.at("o").get_ref<const std::string&>());
+        ticker.high     = std::stod(data.at("h").get_ref<const std::string&>());
+        ticker.low      = std::stod(data.at("l").get_ref<const std::string&>());
+        ticker.volume   = std::stod(data.at("v").get_ref<const std::string&>());
+        ticker.quantity = std::stod(data.at("q").get_ref<const std::string&>());
+        return ticker;
+    }
+
+    void parse()
+    {
+        const std::string content = FileUtilities::ReadFile(getDataDir() / "miniTicker.json");
+        try {
+            const nlohmann::json jsonData = nlohmann::json::parse(content);
+            const nlohmann::json& data = jsonData["data"];
+            {
+                MiniTicker ticker = parseMiniTicker(data);
+                std::cout << ticker << std::endl;
+            }
+            {
+                MiniTicker ticker = parseMiniTicker2(data);
+                std::cout << ticker << std::endl;
+            }
+        }
+        catch (const std::exception& exc) {
+            std::cout << exc.what() << std::endl;
+        }
+    }
+}
+
+namespace binance::all_streams
+{
+    using market_data::BookTicker;
+    using market_data::MiniTicker;
+    struct NoYetImplemented {};
+
+    using BinanceMarketEvent = std::variant<BookTicker, MiniTicker, NoYetImplemented>;
+
+    struct EventHandler
+    {
+        void operator()(const BookTicker& ticker) const {
+            std::cout << ticker << std::endl;
+        }
+        void operator()(const MiniTicker& ticker) const {
+            std::cout << ticker << std::endl;
+        }
+        void operator()(const NoYetImplemented&) const {
+            std::cout << "NoYetImplemented" << std::endl;
+        }
+    };
+
+    void visit_test()
+    {
+        /*
+        template <typename ...Ts>
+        struct overloaded : Ts... {
+            using Ts::operator()...;
+        };
+
+        std::visit(overloaded{
+            [](const auto& x) {std::cout <<  x << "\n";},
+            [](const Other& x) {std::cout << "Other\n";},
+        }, event);
+        */
+    }
+
+    BinanceMarketEvent parse(const nlohmann::json& jsonData)
+    {
+        const std::string& stream = jsonData["stream"].get_ref<const std::string&>();
+        std::cout << stream << std::endl;
+        return  NoYetImplemented{};
+    }
+
+    void allStreams()
+    {
+        EventHandler handler;
+        for (const auto data = readFile(getDataDir() / "allData.json"); const std::string &line : data) {
+            try {
+                const nlohmann::json jsonData = nlohmann::json::parse(line);
+                const BinanceMarketEvent event = parse(jsonData);
+                std::visit(handler, event);
+            }
+            catch (const std::exception& exc) {
+                std::cout << exc.what() << std::endl;
+            }
+        }
+    }
+}
 
 
 void binance::Experiments::TestAll()
 {
-
+    all_streams::allStreams();
+    // parsing::mini_ticker::parse();
 }

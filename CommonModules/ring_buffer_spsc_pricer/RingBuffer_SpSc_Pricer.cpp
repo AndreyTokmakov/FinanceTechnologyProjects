35d800b10ce527460d4a2d91f363402a02900ab8
Andrei.Tokmakov <andtokm@yandex.ru>
2025-11-16 20:21:31 +0400

    16_11_2025_01

--- CommonModules/ring_buffer_spsc_pricer/RingBuffer_SpSc_Pricer.cpp
+++ CommonModules/ring_buffer_spsc_pricer/RingBuffer_SpSc_Pricer.cpp
@@ -8,15 +8,33 @@ Description : RingBuffer_SpSc_Pricer.cpp
 ============================================================================**/
 
 #include "RingBuffer_SpSc_Pricer.hpp"
+#include "StringUtilities.hpp"
+#include "DateTimeUtilities.hpp"
 
-#include <vector>
 #include <cstdint>
+#include <memory>
+#include <cstring>
 #include <iostream>
+#include <ostream>
+#include <utility>
+#include <vector>
 #include <atomic>
-
+#include <thread>
+#include <syncstream>
+#include <random>
+#include <variant>
 
 namespace
 {
+    std::random_device rd{};
+    auto generator = std::mt19937{ rd() };
+
+    int getRandomInRange(const int start, const int end) noexcept
+    {
+        auto distribution = std::uniform_int_distribution<>{ start, end };
+        return distribution(generator);
+    }
+
     constexpr uint32_t fast_modulo(const uint32_t n, const uint32_t d) noexcept {
         return n & (d - 1);
     }
@@ -28,7 +46,6 @@ namespace
 
 namespace
 {
-
     template<typename T, uint32_t Capacity>
     struct RingBuffer
     {
@@ -45,13 +62,12 @@ namespace
         [[nodiscard]]
         bool put(value_type& item)
         {
-            size_type headCached = head.load(std::memory_order::relaxed);
+            const size_type headCached = head.load(std::memory_order::relaxed);
             const size_type headNext = fast_modulo(headCached + 1, Capacity);
             if (headNext == tail.load(std::memory_order::acquire)) {
                 return false;
             }
 
-            item = buffer[headCached];
             buffer[headCached] = item;
             head.store(headNext, std::memory_order::release);
 
@@ -66,7 +82,7 @@ namespace
                 return false;
             }
 
-            item = buffer[tailLocal];
+            item = std::move(buffer[tailLocal]);
             tail.store(fast_modulo(tailLocal + 1, Capacity), std::memory_order::release);
 
             return true;
@@ -99,7 +115,184 @@ namespace
     };
 }
 
+namespace tests
+{
+    struct PriceLevel
+    {
+        PriceLevel() {
+            std::cout << "PriceLevel::PriceLevel()\n";
+        }
+
+        ~PriceLevel() {
+            std::cout << "PriceLevel::~PriceLevel()\n";
+        }
+
+        PriceLevel(const PriceLevel &) {
+            std::cout << "PriceLevel(const PriceLevel &)\n";
+        }
+
+        PriceLevel(PriceLevel &&) noexcept {
+            std::cout << "PriceLevel(PriceLevel &&) noexcept\n";
+        }
+
+        PriceLevel & operator=(const PriceLevel&) {
+            std::cout << " PriceLevel & operator=(const PriceLevel&)\n";
+            return *this;
+        }
+
+        PriceLevel & operator=(PriceLevel &&) noexcept {
+            std::cout << "PriceLevel & operator=(PriceLevel &&) noexcept\n";
+            return *this;
+        }
+    };
+
+    struct Trade {};
+    struct Ticker {};
+    struct BookUpdate
+    {
+        std::vector<PriceLevel> bids;
+        std::vector<PriceLevel> asks;
+    };
+
+    using MarketEvent = std::variant<Trade, BookUpdate, Ticker>;
+
+    struct EventHandler
+    {
+        void operator()(const Trade&) const {
+            std::cout << "===> Trade" << std::endl;
+        }
+        void operator()(const Ticker&) const {
+            std::cout << "===> Ticker" << std::endl;
+        }
+        void operator()(const BookUpdate& update) const {
+            std::cout << "===> BookUpdate: size = {" << update.asks.size() << ", " << update.asks.size() << "}\n";
+        }
+    };
+
+    BookUpdate makeBookUpdate()
+    {
+        BookUpdate update;
+
+        update.bids.reserve(2);
+        update.bids.emplace_back();
+        update.bids.emplace_back();
+
+
+        update.asks.reserve(2);
+        update.asks.emplace_back();
+        update.asks.emplace_back();
+
+        return update;
+    }
+
+    Trade makeTrade()
+    {
+        Trade trade;
+        return trade;
+    }
+
+    Ticker makeTicker()
+    {
+        Ticker ticker;
+        return ticker;
+    }
+
+    uint32_t counter = 0;
+
+    MarketEvent getEvent()
+    {
+        std::cout << "getEvent: " << counter << std::endl;
+        ++counter;
+        counter = counter % 3;
+
+
+        if (0 == counter)
+            return makeBookUpdate();
+        if (1 == counter)
+            return makeTrade();
+
+        return makeTicker();
+    }
+}
+
+namespace tests
+{
+    void processStringTest()
+    {
+        RingBuffer<std::string, 1024> queue {};
+
+        auto produce = [&queue]
+        {
+            while (true)
+            {
+                const int32_t size = getRandomInRange(0, 1024);
+                const std::string text = StringUtilities::randomString(size);
+                std::osyncstream { std::cout } << DateTimeUtilities::getCurrentTime() << "Producer: " << size
+                    << " bytes written\n" << text << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds (100U));
+            }
+        };
+
+        auto consume = [&queue]
+        {
+            while (true)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds (100U));
+            }
+        };
+
+        std::jthread producer { produce },
+                     consumer { consume };
+    }
+}
+
+namespace tests
+{
+    void processEventsTests()
+    {
+        RingBuffer<MarketEvent, 16> queue {};
+
+        auto produce = [&queue]
+        {
+            while (true)
+            {
+                MarketEvent event = getEvent();
+                queue.put(event);
+                std::osyncstream { std::cout } << DateTimeUtilities::getCurrentTime() << "added" << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds (250U));
+            }
+        };
+
+        auto consume = [&queue]
+        {
+            EventHandler eventHandler;
+            MarketEvent event;
+            while (true)
+            {
+                if (queue.pop(event)) {
+                    std::visit(eventHandler, event);
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds (1u));
+            }
+        };
+
+        std::jthread producer { produce },
+                     consumer { consume };
+    }
+}
+
+
 void RingBuffer_SpSc_Pricer::TestAll()
 {
+    // tests::processStringTest();
+    // tests::processEventsTests();
 
+    tests::BookUpdate update1 = tests::makeBookUpdate();
+
+    std::cout << "===> BookUpdate1: size = {" << update1.asks.size() << ", " << update1.asks.size() << "}\n";
+
+    tests::BookUpdate update2 = std::move(update1);
+
+    std::cout << "===> BookUpdate2: size = {" << update2.asks.size() << ", " << update2.asks.size() << "}\n";
+    std::cout << "===> BookUpdate1: size = {" << update1.asks.size() << ", " << update1.asks.size() << "}\n";
 }

9c0642a378709eb27389d8e560b3fb540d13a595
Andrei.Tokmakov <andtokm@yandex.ru>
2025-11-26 19:22:13 +0400

    26_11_2025_03

---
+++ Utilities/Testing.hpp
@@ -0,0 +1,180 @@
+/**============================================================================
+Name        : Testing.hpp
+Created on  : 26.11.2025
+Author      : Andrei Tokmakov
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : Testing.hpp
+============================================================================**/
+
+#ifndef CPPPROJECTS_TESTING_HPP
+#define CPPPROJECTS_TESTING_HPP
+
+#include <expected>
+#include <iostream>
+#include <source_location>
+#include <print>
+
+namespace testing
+{
+    enum class Action
+    {
+        Continue,
+        Terminate
+    };
+
+    constexpr void performAction(const Action action)
+    {
+        if ( Action::Terminate == action)
+        {
+            std::terminate();
+        }
+    }
+
+    constexpr void printLocation(const std::source_location& location)
+    {
+        std::println(std::cerr, "\tFile: {}\n\tFunction: {}\n\tLine: {}",
+            location.file_name(), location.function_name(), location.line());
+    }
+
+    // TODO: Add concepts to 'Ty'
+    template<typename Ty>
+    constexpr void AssertEqual(const Ty& expected,
+                               const Ty& actual,
+                               const Action action = Action::Terminate,
+                               const std::source_location& location = std::source_location::current())
+    {
+        if (expected != actual)
+        {
+            std::println(std::cerr, "Assertation failed: {} != {}",expected, actual);
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    // TODO: Add concepts to 'Ty'
+    template<typename Ty>
+    constexpr void AssertEqual(const Ty& expected,
+                               const Ty& actual,
+                               const std::string_view message,
+                               const Action action = Action::Terminate,
+                               const std::source_location& location = std::source_location::current())
+    {
+        if (expected != actual)
+        {
+            std::println(std::cerr, "{}: {} != {}", message, expected, actual);
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    // TODO: Add concepts to 'Ty'
+    template<typename Ty>
+    constexpr void AssertNotNull(const Ty* actual,
+                                 const Action action = Action::Terminate,
+                                 const std::source_location& location = std::source_location::current())
+    {
+        if (nullptr == actual)
+        {
+            std::println(std::cerr, "Assertion failed (Actual value is null)");
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    // TODO: Add concepts to 'Ty'
+    template<typename Ty>
+    constexpr void AssertNotNull(const Ty* actual,
+                                 const std::string_view message,
+                                 const Action action = Action::Terminate,
+                                 const std::source_location& location = std::source_location::current())
+    {
+        if (nullptr == actual)
+        {
+            std::println(std::cerr, "{}", message);
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    // TODO: Add concepts to 'Ty'
+    template<typename Ty>
+    constexpr void AssertIsNull(const Ty* actual,
+                                const Action action = Action::Terminate,
+                                const std::source_location& location = std::source_location::current())
+    {
+        if (nullptr != actual)
+        {
+            std::println(std::cerr, "Assertion failed (Actual value is not null)");
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    // TODO: Add concepts to 'Ty'
+    template<typename Ty>
+    constexpr void AssertIsNull(const Ty* actual,
+                                const std::string_view message,
+                                const Action action = Action::Terminate,
+                                const std::source_location& location = std::source_location::current())
+    {
+        if (nullptr != actual)
+        {
+            std::println(std::cerr, "{}",message);
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    constexpr void AssertTrue(const bool condition,
+                              const Action action = Action::Terminate,
+                              const std::source_location& location = std::source_location::current())
+    {
+        if (!condition)
+        {
+            std::println(std::cerr, "Assertation failed");
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    constexpr void AssertTrue(const bool condition,
+                              const std::string_view message,
+                              const Action action = Action::Terminate,
+                              const std::source_location& location = std::source_location::current())
+    {
+        if (!condition)
+        {
+            std::println(std::cerr, "{}", message);
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    constexpr void AssertFalse(const bool condition,
+                               const Action action = Action::Terminate,
+                               const std::source_location& location = std::source_location::current())
+    {
+        if (condition)
+        {
+            std::println(std::cerr, "Condition is True (False expected):");
+            printLocation(location);
+            performAction(action);
+        }
+    }
+
+    constexpr void AssertFalse(const bool condition,
+                               const std::string_view message,
+                               const Action action = Action::Terminate,
+                               const std::source_location& location = std::source_location::current())
+    {
+        if (condition)
+        {
+            std::println(std::cerr, "{}", message);
+            printLocation(location);
+            performAction(action);
+        }
+    }
+}
+
+#endif //CPPPROJECTS_TESTING_HPP

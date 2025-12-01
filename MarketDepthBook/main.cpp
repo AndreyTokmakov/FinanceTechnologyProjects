052f906ebea0cacddcb0fb7c6280b0f9daefd7f9
Andrei.Tokmakov <andtokm@yandex.ru>
2025-12-01 11:30:29 +0400

    01_12_2025_01

--- MarketDepthBook/main.cpp
+++ MarketDepthBook/main.cpp
@@ -11,8 +11,12 @@ Description :
 #include <iostream>
 #include <string_view>
 #include <optional>
+#include <format>
+#include <iomanip>
+
 
 #include "FlatMap.hpp"
+#include "Testing.hpp"
 
 
 namespace
@@ -31,17 +35,18 @@ namespace
 struct MarketDepthBook
 {
     constexpr static size_t MaxDepth { 1'000 };
-    using Price = double;
+    using Price    = double;
     using Quantity = double;
 
-    flat_map::FlatMap<Price, Quantity, flat_map::SortOrder::Descending> asks { MaxDepth };
-    flat_map::FlatMap<Price, Quantity, flat_map::SortOrder::Ascending>  bids { MaxDepth };
+    flat_map::FlatMap<Price, Quantity, flat_map::SortOrder::Ascending>   asks { MaxDepth };
+    flat_map::FlatMap<Price, Quantity, flat_map::SortOrder::Descending>  bids { MaxDepth };
 
     void buyUpdate(const Price price, const Quantity quantity)
     {
         if (quantity != 0) {
-            const auto result = bids.push(price, quantity);
-            result->value = quantity;
+            if (const auto result = bids.push(price, quantity)) {
+                result->value = quantity;
+            }
         }
         else {
             bids.erase(price);
@@ -51,8 +56,9 @@ struct MarketDepthBook
     void askUpdate(const Price price, const Quantity quantity)
     {
         if (quantity != 0) {
-            const auto result = asks.push(price, quantity);
-            result->value = quantity;
+            if (const auto result = asks.push(price, quantity)) {
+                result->value = quantity;
+            }
         }
         else {
             asks.erase(price);
@@ -76,8 +82,72 @@ struct MarketDepthBook
     //  - number of asks / bids ?
 };
 
+namespace printer
+{
+    using namespace std::string_view_literals;
+
+    static constexpr std::string_view red   = "\033[31m"sv;
+    static constexpr std::string_view green = "\033[32m"sv;
+    static constexpr std::string_view reset = "\033[0m"sv;
+    static constexpr uint32_t tableWidth = 45;
+
+    struct Printer
+    {
+        static void print(const MarketDepthBook& depthBook)
+        {
+            const uint16_t bidSize = depthBook.bids.size(), askSize = depthBook.asks.size();
+
+            std::cout << std::string(tableWidth, '-') << "\n|";
+            std::cout << green << "    Bids: " << std::format("{: ^9}", bidSize);
+            std::cout << reset << "  |  ";
+            std::cout << red   << "    Asks: " << std::format("{: ^9}", askSize);
+            std::cout << reset << "|\n";
+
+            std::cout << std::string(tableWidth, '-') << std::endl;
+            for (uint32_t bIdx = 0, sIdx = 0; bIdx < bidSize || sIdx < askSize; ++bIdx, ++sIdx)
+            {
+                std::cout << "| ";
+
+                if (bidSize > bIdx)
+                {
+                    const auto [price, quantity] = depthBook.bids.data()[bIdx];
+                    std::cout << std::format("{:{}.{}}", price, 8, 6);
+                    std::cout << " : ";
+                    std::cout << std::format("{:{}.{}}", quantity, 8, 6);
+                }
+                else {
+                    std::cout << std::string(21, ' ');
+                }
+
+                std::cout << " | ";
+
+                if (askSize > sIdx)
+                {
+                    const auto [price, quantity] = depthBook.asks.data()[sIdx];
+                    std::cout << std::format("{:{}.{}}", price, 8, 6);
+                    std::cout << " : ";
+                    std::cout << std::format("{:{}.{}}", quantity, 8, 6);
+                }
+                else {
+                    std::cout << std::string(19, ' ');
+                }
+
+                std::cout << " |\n";
+            }
+            std::cout << std::string(tableWidth, '-') << std::endl;
+        }
+    };
+}
+
 namespace testing
 {
+    using namespace ::testing;
+
+    void print(const MarketDepthBook& depthBook)
+    {
+        printer::Printer::print(depthBook);
+    }
+
     enum class Side {
         Buy,
         Sell
@@ -109,29 +179,171 @@ namespace testing
         }
     }
 
-    void test1()
+    void test_Bid_Empty()
     {
         MarketDepthBook book;
-        handleEvents(book, { { 100.0, 100.0, Side::Buy } });
+        handleEvents(book, {});
+
+        const std::optional<std::pair<double, double>> bestBid = book.getBestBid();
+        AssertFalse(bestBid.has_value());
+    }
+
+    void test_Bid_SingleBuy()
+    {
+        MarketDepthBook book;
+        handleEvents(book, {
+            { 101.01, 10.01, Side::Buy },
+        });
+
+        const std::optional<std::pair<double, double>> bestBid = book.getBestBid();
+        AssertTrue(bestBid.has_value());
+
+        const auto [price, quantity] = bestBid.value();
+        AssertEqual(101.01, price);
+        AssertEqual(10.01, quantity);
+    }
+
+    void test_Bid_MultipleBuy()
+    {
+        MarketDepthBook book;
+        handleEvents(book, {
+            { 100.0, 10.01, Side::Buy },
+            { 101.0, 11.01, Side::Buy },
+            { 102.02, 12.07, Side::Buy },
+        });
+
+        const std::optional<std::pair<double, double>> bestBid = book.getBestBid();
+        AssertTrue(bestBid.has_value());
+
+        const auto [price, quantity] = bestBid.value();
+        AssertEqual(102.02, price);
+        AssertEqual(12.07, quantity);
+    }
+
+    void test_Bid_UpdateBB()
+    {
+        MarketDepthBook book;
+        handleEvents(book, {
+            { 100.0, 10.01, Side::Buy },
+            { 101.0, 11.01, Side::Buy },
+            { 102.02, 12.07, Side::Buy },
+            { 101.6, 11.01, Side::Buy },
+            { 102.02, 12.01, Side::Buy },
+
+        });
+
+        const std::optional<std::pair<double, double>> bestBid = book.getBestBid();
+        AssertTrue(bestBid.has_value());
+
+        const auto [price, quantity] = bestBid.value();
+        AssertEqual(102.02, price);
+        AssertEqual(12.01, quantity);
+    }
+
+    void test_Bid_Delete_BB()
+    {
+        MarketDepthBook book;
+        handleEvents(book, {
+            { 100.0, 10.01, Side::Buy },
+            { 101.0, 11.01, Side::Buy },
+            { 102.02, 12.07, Side::Buy },
+            { 101.6, 11.011, Side::Buy },
+            { 102.02, 0, Side::Buy },
+
+        });
+
+        const std::optional<std::pair<double, double>> bestBid = book.getBestBid();
+        AssertTrue(bestBid.has_value());
+
+        const auto [price, quantity] = bestBid.value();
+        AssertEqual(101.6, price);
+        AssertEqual(11.011, quantity);
+    }
+
+    void test_Bid_Delete_Non_BB()
+    {
+        MarketDepthBook book;
+        handleEvents(book, {
+            { 100.00, 10.01, Side::Buy },
+            { 101.00, 11.01, Side::Buy },
+            { 102.02, 12.07, Side::Buy },
+            { 101.60, 11.011, Side::Buy }
+        });
+
+        const uint32_t bidsSize = book.bids.size();
+
+        handleEvents(book, {
+             { 101.00, 0, Side::Buy },
+        });
+
+        AssertEqual(bidsSize - 1, book.bids.size());
+
+
+        const std::optional<std::pair<double, double>> bestBid = book.getBestBid();
+        AssertTrue(bestBid.has_value());
+
+        const auto [price, quantity] = bestBid.value();
+        AssertEqual(102.02, price);
+        AssertEqual(12.07, quantity);
+    }
+
+    void test_Ask_1()
+    {
+        MarketDepthBook book;
+        handleEvents(book, {
+            { 101.0, 11.0, Side::Sell },
+            { 102.0, 12.0, Side::Sell },
+        });
+        print(book);
+    }
+
+    void test_load()
+    {
+        MarketDepthBook book;
+
+        double buyPrice = 100.0, sellPrice = 200.0;
+        double buyQuantity = 10.0, sellQuantity = 10.0;
+        for (int i = 0; i < 100; ++i)
+        {
+            handleUpdate(book, DepthUpdate { .price=buyPrice, .quantity=buyQuantity, .side= Side::Buy });
+            buyPrice += 0.001;
+            buyQuantity += 0.0001;
+
+            handleUpdate(book, DepthUpdate { .price=sellPrice, .quantity=sellQuantity, .side= Side::Sell });
+            sellPrice -= 0.001;
+            sellQuantity -= 0.0001;
+        }
+
+        print(book);
     }
 }
 
 
-int main([[maybe_unused]] int argc,
-         [[maybe_unused]] char** argv)
+// TODO: BID
+//  + Empty - no Best Buy
+//  + Single Buy - check Best Buy
+//  + Multiple Buy - Check BB
+//  + Update Exising BB
+//  + Delete Exising (BB) Buy - Check BB
+//  + Delete Exising (non BB) Buy - Check BB
+
+int main([[maybe_unused]] const int argc,
+         [[maybe_unused]] char** argv,
+         [[maybe_unused]] char** env)
 {
     const std::vector<std::string_view> args(argv + 1, argv + argc);
 
-    MarketDepthBook book;
+    testing::test_Bid_Empty();
+    testing::test_Bid_SingleBuy();
+    testing::test_Bid_MultipleBuy();
+    testing::test_Bid_UpdateBB();
+    testing::test_Bid_Delete_BB();
+    testing::test_Bid_Delete_Non_BB();
 
-    book.buyUpdate(100.0, 100.0);
-    std::cout << book.bids << std::endl;
 
-    book.buyUpdate(100.0, 22.0);
-    std::cout << book.bids << std::endl;
 
-    book.buyUpdate(100.0, 11.0);
-    std::cout << book.bids << std::endl;
+    // testing::test_Ask_1();
+    // testing::test_load();
 
 
     return EXIT_SUCCESS;
